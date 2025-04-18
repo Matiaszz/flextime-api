b61f87d3d2182cf04c2b05fa1252a79d34fa5e21
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-04-18 12:39:30 -0300

    chore: init

---
+++ src/main/java/dev/matias/flextime/api/ApiApplication.java
@@ -0,0 +1,19 @@
+package dev.matias.flextime.api;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MYSQL_DATABASE", dotenv.get("MYSQL_DATABASE"));
+		System.setProperty("MYSQL_USER", dotenv.get("MYSQL_USER"));
+		System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
